a069a99ba2d1c6905ba5808f0d4bff2d9646ff61
David Sterba <dsterba@suse.cz>
2014-10-13 11:13:55 +0200

    fuzzer-sb: add loop start point
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- fuzzer-sb/fuzzer-test.sh
+++ fuzzer-sb/fuzzer-test.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 loops=10
+start=${1:-1}
 mnt=mnt
 
 mkdir -p $mnt
 
-for i in `seq $loops`; do
+for i in `seq $start $loops`; do
 	echo Loop $i
 	cp --sparse=always img.orig img.test
 	echo Fuzz
